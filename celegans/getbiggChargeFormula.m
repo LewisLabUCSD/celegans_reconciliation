0f199a5e754493ccb11f6ae7ac457972c24d79e1
sean1292 <ssadykof@ucsd.edu>
2017-01-31 12:00:12 -0800

    Added Gibbs Free Energy retrieval  from Metacyc
    Doesnt fully work

--- celegans/getbiggChargeFormula.m
+++ celegans/getbiggChargeFormula.m
@@ -1,4 +1,4 @@
-function metBiGG = getbiggChargeFormula(all_bigg, disp)
+function metBiGG = getbiggChargeFormula(all_biggid, disp)
 % metBiGG = getbiggChargeFormula(all_bigg, 1)
 % INPUT: 
 % all_bigg: list of Bigg compound ids
@@ -7,21 +7,49 @@ function metBiGG = getbiggChargeFormula(all_bigg, disp)
 % metBiGG: list of Chemical Formulas and Charges from
 %          Bigg 
 tic;
-metBiGG = cell(length(all_bigg),5);
-options = weboptions('Timeout',30, 'RequestMethod', 'GET');
-prev='';
+metBiGG = cell(length(all_biggid),9);
+options = weboptions('Timeout',30);
+
 notInbigg = 0;
 noCharge=0;
 noFormula=0;
+inchiFound = 0;
+chResolved = 0;
+nochResolved = 0;
+formResolved = 0;
+noformResolved = 0;
+
 %check all Bigg IDs
-for i=1:length(all_bigg)
-    metBiGG{i,1}= all_bigg{i,1};
+for i=1:length(all_biggid)
+    metBiGG{i,1}= all_biggid{i,1};
+                
+    data= webread(strcat('http://bigg.ucsd.edu/api/v2/universal/metabolites/', all_biggid{i,1}(1:end-3)),options);
+
+    if isfield(data.database_links,'BioCyc')
+        data.database_links.BioCyc.id
+        metaCyc = webread( strcat('http://websvc.biocyc.org/getxml?id=', data.database_links.BioCyc.id), options);
+        regex = regexp(metaCyc, '<gibbs-0 datatype="float" units="kcal\/mol"\>(-?\d+\.\d+)', 'tokens');
+        if isempty(regex)~=1
+            gibbs = regex{1};
+            display(gibbs);
+        end
+    end
     try
         %if not same id as previous one
-        if strcmp(prev, all_bigg{i,1}(1:end-3))~=1
+
            
-            prev = all_bigg{i,1}(1:end-3);
-            data= webread(strcat('http://bigg.ucsd.edu/api/v2/universal/metabolites/', all_bigg{i,1}(1:end-3)),options);
+            
+            data= webread(strcat('http://bigg.ucsd.edu/api/v2/universal/metabolites/', all_biggid{i,1}(1:end-3)),options);
+            
+            if isfield(data.database_links,'BioCyc')
+                metaCyc = webread( data.database_links.BioCyc.link, options);
+                regex = regexp(metaCyc, '<gibbs-0 datatype="float" units="kcal\/mol"\>(-?\d+\.\d+)', 'tokens');
+                if isempty(regex)~=1
+                    gibbs = regex{1};
+                    display(gibbs);
+                    metBiGG{i,9} = gibbs;
+                end
+            end
             
             %if there is a charge in BIGG
             if isempty(data.charges)~=1
@@ -33,11 +61,44 @@ for i=1:length(all_bigg)
                     else
                         metBiGG{i,2} = strcat(metBiGG{i,2}, num2str(data.charges(k,1)), ', ');
                         metBiGG{i,4} = length(data.charges);
+                       
+                        if isfield(data.database_links,'MetaNetX_MNX_Chemical')
+                            metaNetX = webread( data.database_links.MetaNetX_MNX_Chemical.link);
+                            regex = regexp(metaNetX, 'InChI=(.+)</td></tr>   <tr><td', 'tokens');
+                            if isempty(regex)~=1
+                                inchi = regex{1};
+                            end
+                        end
+                        if isempty(inchi)~=1
+                             metBiGG{i,6} = inchi;
+                             inchiFound = inchiFound +1;
+                        end
+                        
+                        if  isempty(metBiGG{i,6})~=1
+                            metBiGG{i,7} = getChargeFromInChI(metBiGG{i,6});
+                            chResolved = chResolved+1;
+                        end
                     end
                 end 
             else
+                if isfield(data.database_links,'MetaNetX_MNX_Chemical')
+                    metaNetX = webread( data.database_links.MetaNetX_MNX_Chemical.link);
+                    regex = regexp(metaNetX, 'InChI=(.+)</td></tr>   <tr><td', 'tokens');
+                    if isempty(regex)~=1
+                        inchi = regex{1};
+                    end
+                end
+                if isempty(inchi)~=1
+                     metBiGG{i,6} = inchi;
+                     inchiFound = inchiFound +1;
+                end
+                
                 noCharge = noCharge+1;
                 metBiGG{i,2} = 'No Charge In BiGG';
+                if  isempty(metBiGG{i,6})~=1
+                    metBiGG{i,7} = getChargeFromInChI(metBiGG{i,6});
+                    nochResolved = nochResolved+1;
+                end
             end
                
             %if there is a Formula in BIGG
@@ -50,15 +111,50 @@ for i=1:length(all_bigg)
                     else
                         metBiGG{i,3} = strcat(metBiGG{i,3}, data.formulae{k,1}, ', ');
                         metBiGG{i,5} = length(data.formulae);
+                        
+                       if isfield(data.database_links,'MetaNetX_MNX_Chemical')
+                            metaNetX = webread( data.database_links.MetaNetX_MNX_Chemical.link);
+                            regex = regexp(metaNetX, 'InChI=(.+)</td></tr>   <tr><td', 'tokens');
+                            if isempty(regex)~=1
+                                inchi = regex{1};
+                            end
+                        end
+                        if isempty(inchi)~=1
+                             metBiGG{i,6} = inchi;
+                             inchiFound = inchiFound +1;
+                        end
+                        
+                        if  isempty(metBiGG{i,6})~=1
+                            metBiGG{i,8} = getFormulaFromInChI(metBiGG{i,6});
+                            formResolved = formResolved+1;
+                        end
                     end
                 end
             else
+                
+                if isfield(data.database_links,'MetaNetX_MNX_Chemical')
+                    metaNetX = webread( data.database_links.MetaNetX_MNX_Chemical.link);
+                    regex = regexp(metaNetX, 'InChI=(.+)</td></tr>   <tr><td', 'tokens');
+                    if isempty(regex)~=1
+                        inchi = regex{1};
+                    end
+                end
+                if isempty(inchi)~=1
+                     metBiGG{i,6} = inchi;
+                     inchiFound = inchiFound +1;
+                end
+                
                 noFormula = noFormula+1;
                 metBiGG{i,3} = 'No formula In BiGG';
+                
+                if  isempty(metBiGG{i,6})~=1
+                    metBiGG{i,8} = getFormulaFromInChI(metBiGG{i,6});
+                    noformResolved = noformResolved+1;
+                end
             end
             %checking if display flag is set to ON
             if disp == 1
-                fprintf('\n%d. Looking for Formula and Charge for %s: ',i,all_bigg{i,1}(1:end-3));
+                fprintf('\n%d. Looking for Formula and Charge for %s: ',i,all_biggid{i,1}(1:end-3));
                 if isempty(data.formulae)~=1
                     fprintf('\nFormula: ');
                     for k=1:length(data.formulae)
@@ -80,26 +176,25 @@ for i=1:length(all_bigg)
 
             end
         
-        %ID is same as previous one    
-        else
-            metBiGG{i,2}= metBiGG{i-1,2};
-            metBiGG{i,3}= metBiGG{i-1,3}; 
-        end
+  
+
     %case where webread returns error    
     catch
         if disp == 1
             notInbigg = notInbigg+1;
             metBiGG{i,2}= 'Not found';
             metBiGG{i,3}= 'Not found';
-            fprintf('\n%d. Looking for Formula and Charge for %s: ',i,all_bigg{i,1}(1:end-3));
+            fprintf('\n%d. Looking for Formula and Charge for %s: ',i,all_biggid{i,1}(1:end-3));
             fprintf('\n%s.', 'Not found')
         end
     end
-if i == length(all_bigg)   
+if i == length(all_biggid)   
 fprintf('\nSummary:\n');
 fprintf('%d Metabolites were not in the BiGG database\n',notInbigg);
-fprintf('%d Metabolites did not have charge specified in the BiGG database\n',noCharge);
-fprintf('%d Metabolites did not have a formula specified in the BiGG database\n',noFormula);
+fprintf('%d Metabolites did not have charge specified in the BiGG database (%d Resolved using InChi)\n',noCharge, nochResolved);
+fprintf('%d Metabolites did not have a formula specified in the BiGG database (%d Resolved using InChi)\n',noFormula, noformResolved);
+fprintf('%d Charges Resolved using InChi ID\n',chResolved);
+fprintf('%d Formulas Resolved using InChi ID\n',formResolved);
 end
 end
 toc;
